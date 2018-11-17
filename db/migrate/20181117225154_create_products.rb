9297d4029bb1d55e1e31e8bfa3b56f354b50dfd9
Katrina Tran <kat.c.tran@gmail.com>
2018-11-17 15:52:14 -0700

    product_model

---
+++ db/migrate/20181117225154_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.text :description
+      t.string :title
+      t.decimal :price
+      t.string :product_number
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+    description { "MyText" }
+    title { "MyString" }
+    price { "9.99" }
+    product_number { "MyString" }
+  end
+end
