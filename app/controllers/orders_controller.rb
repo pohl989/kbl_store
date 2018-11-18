a32759931359d61cd099253f856dfdb4815e4ec3
Katrina Tran <kat.c.tran@gmail.com>
2018-11-18 10:29:55 -0700

    CRUD order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,40 @@
 class OrdersController < ApplicationController
   def index
+    render json: Order.all
   end
 
+  def create
+    order = Order.new(order_params)
+    if order.save
+      render json: order
+    else
+      render json: {error: order.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    order = Order.find(params[:id])
+    order.update(complete: !item.complete)
+    render json: order
+  end
+
+
+
   def show
+
   end
 
   def new
+
   end
 
   def edit
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_number, :status, :order_date)
+  end 
+
 end
