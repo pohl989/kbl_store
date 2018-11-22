79c6eaddeec4af28c4be9a89196d1cc2d9924eb7
pohl989 <pohl989@gmail.com>
2018-11-21 21:22:16 -0700

    mapped the state to props

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
 import { getProducts } from '../../reducers/product'
 import { connect } from 'react-redux'
+import { List, Icon, Button, Label, Segment, Header} from 'semantic-ui-react';
 
 class Product extends Component {
 
@@ -12,10 +12,34 @@ class Product extends Component {
 
 
   render() {
+
+    const { products } = this.props
+
     return (
       <div>
         <Header as='h1' textAlign='center'>Product Component</Header>
-
+        <Segment inverted>
+          <List divided inverted relaxed verticalAlign='middle'>
+          { products.map( single => 
+            <List.Item key={single.id}>
+              <List.Content floated='right'>
+                  {single.title}
+                </List.Content>
+              <List.Content>
+                <Label style={{paddingLeft: '15px'}} color="blue" image>
+                  {single.price}
+                </Label>
+                <div style={{paddingLeft: '20px', display: 'inline-flex'}}>
+                  <Header as="h5" >Price: ${single.price}</Header>
+                </div>
+                <div style={{paddingLeft: '20px', display: 'inline-flex'}}>
+                  {/* <Header as="h5" color="orange">${single.price_per_person}/person</Header> */}
+                </div>
+              </List.Content>
+            </List.Item>
+              )}
+          </List>
+        </Segment>
       </div>
       
     );
@@ -23,7 +47,7 @@ class Product extends Component {
 }
 
 const mapStateToProps = state => {
-  return { products: state.products };
+  return { products: state.product };
 };
 
 export default connect(mapStateToProps)(Product);
