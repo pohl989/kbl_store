fe117146747db25799d335b3a5e00341c40f233c
pohl989 <pohl989@gmail.com>
2018-11-22 12:18:57 -0700

    added cards

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { getProducts } from '../../reducers/product'
 import { connect } from 'react-redux'
-import { List, Icon, Button, Label, Segment, Header} from 'semantic-ui-react';
+import { Card, Icon, Image, Segment, Header} from 'semantic-ui-react';
 
 class Product extends Component {
 
@@ -18,27 +18,23 @@ class Product extends Component {
     return (
       <div>
         <Header as='h1' textAlign='center'>Product Component</Header>
-        <Segment inverted>
-          <List divided inverted relaxed verticalAlign='middle'>
-          { products.map( single => 
-            <List.Item key={single.id}>
-              <List.Content floated='right'>
-                  {single.title}
-                </List.Content>
-              <List.Content>
-                <Label style={{paddingLeft: '15px'}} color="blue" image>
-                  {single.price}
-                </Label>
-                <div style={{paddingLeft: '20px', display: 'inline-flex'}}>
-                  <Header as="h5" >Price: ${single.price}</Header>
-                </div>
-                <div style={{paddingLeft: '20px', display: 'inline-flex'}}>
-                  {/* <Header as="h5" color="orange">${single.price_per_person}/person</Header> */}
-                </div>
-              </List.Content>
-            </List.Item>
-              )}
-          </List>
+        <Segment >
+          <Card.Group centered doubling>
+            { products.map( single => 
+              <Card key={single.id}>
+                <Image src={single.image} />
+                <Card.Content>
+                  <Card.Header>{single.title}</Card.Header>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    ${single.price}
+                  </a>
+                </Card.Content>
+              </Card>
+            )}
+          </Card.Group>
         </Segment>
       </div>
       
