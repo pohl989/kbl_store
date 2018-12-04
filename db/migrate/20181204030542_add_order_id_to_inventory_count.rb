827ad023fc78928078d71853934196f8fcf63ec3
Katrina Tran <kat.c.tran@gmail.com>
2018-12-03 20:19:50 -0700

    inventory count seeds

--- db/migrate/20181204030542_add_order_id_to_inventory_count.rb
+++ db/migrate/20181204030542_add_order_id_to_inventory_count.rb
@@ -1,5 +1,5 @@
 class AddOrderIdToInventoryCount < ActiveRecord::Migration[5.2]
   def change
-    add_column :inventory_counts, :order, :integer, index: true 
+    add_column :inventory_counts, :order_id, :integer, index: true 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_030542) do
     t.string "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "order"
+    t.integer "order_id"
   end
 
   create_table "order_items", force: :cascade do |t|
