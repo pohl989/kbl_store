481483aca620651490b7e5ea754deb97898396b5
pohl989 <pohl989@gmail.com>
2018-12-08 15:50:09 -0700

    fixed bugs in migrations

--- db/migrate/20181129052525_add_fields_to_product.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldsToProduct < ActiveRecord::Migration[5.2]
-  def change
-    add_column :products, :image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_030542) do
+ActiveRecord::Schema.define(version: 2018_12_05_043946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "inventory_counts", force: :cascade do |t|
     t.string "title"
@@ -103,6 +118,8 @@ ActiveRecord::Schema.define(version: 2018_12_04_030542) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "package_items", "packages"
